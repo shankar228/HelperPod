ee15b2e70eab7f31cd439e9d08f199c3a0c51659
Shankar Kumar <shankar.kumar@decimal.co.in>
2018-07-30 13:17:38 +0530

    Framwork created

---
+++ HelperPod/HelperPod/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  HelperPod
+//
+//  Created by Shankar Kumar on 30/07/18.
+//  Copyright © 2018 shankar kumar. All rights reserved.
+//
+
+import Foundation
+
+public class Helper {
+    public static func name() -> String {
+        return "Helper"
+    }
+}
---
+++ HelperPod/HelperPod/HelperPod.h
@@ -0,0 +1,19 @@
+//
+//  HelperPod.h
+//  HelperPod
+//
+//  Created by Shankar Kumar on 30/07/18.
+//  Copyright © 2018 shankar kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelperPod.
+FOUNDATION_EXPORT double HelperPodVersionNumber;
+
+//! Project version string for HelperPod.
+FOUNDATION_EXPORT const unsigned char HelperPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelperPod/PublicHeader.h>
+
+
